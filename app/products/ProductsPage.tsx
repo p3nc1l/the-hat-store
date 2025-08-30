2ae6e44a2d3aada4463838e8be70fff85bbb1e35
p3nc1l <szocsdavid598@gmail.com>
2025-08-30 19:38:48 +0300

    added support for multiple images in table

--- app/products/ProductsPage.tsx
+++ app/products/ProductsPage.tsx
@@ -38,7 +38,7 @@ const ProductsPage = async (props: { categoryName?: string } ) => {
               <CardHeader>
                 <AspectRatio ratio={1 / 1} className="flex items-center justify-center bg-neutral-100 dark:bg-neutral-800">
                   <span>No image</span>
-                  {product.imageUrl && <Image src={product.imageUrl} alt={product.name} layout="fill" objectFit="cover" />}
+                  {product.imageUrls && <Image src={product.imageUrls[0]} alt={product.name} layout="fill" objectFit="cover" />}
                 </AspectRatio>
               </CardHeader>
               <CardContent>
@@ -46,7 +46,11 @@ const ProductsPage = async (props: { categoryName?: string } ) => {
               </CardContent>
               <CardFooter className="flex justify-between">
                 <div>{product.price} $</div>
-                <Button>Add to Cart</Button>
+                <Button size={"icon"} aria-label="Add to cart">
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
+                  </svg>
+                </Button>
               </CardFooter>
             </Card>
           </Link>
